0e5a4f77b0121d20df9ee1f49fbe3829f8de7055
indoorsglobal <108294345+rajurao1106@users.noreply.github.com>
2026-05-23 15:55:53 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "krd-admin-backend-five.vercel.app",
       },
+       {
+        protocol: "https",
+        hostname: "krd-admin-backend-five",
+      },
       
     ],
   },
