5da4272e21d34a4d1b4d753b1afa5af59fb13292
indoorsglobal <108294345+rajurao1106@users.noreply.github.com>
2026-05-23 16:01:44 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -4,15 +4,15 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
+        protocol: "https",
+        hostname: "krd-admin-backend-five.vercel.app",
         port: "",
         pathname: "/uploads/**",
       },
       {
-        protocol: "https",
-        hostname: "krd-admin-backend-five.vercel.app",
-        port: "",
+        protocol: "http",
+        hostname: "localhost",
+        port: "3000",
         pathname: "/uploads/**",
       },
     ],
@@ -20,14 +20,19 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Apply CORS to all API routes
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,POST,PUT,DELETE,OPTIONS" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
           {
             key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization",
           },
         ],
       },
